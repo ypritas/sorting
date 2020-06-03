f6b00e9d5666a32973eb3e538ce59ad1318f16db
Tomas <tomas.bartnykas@gmail.com>
2020-06-03 19:23:24 +0200

    Small changes

--- src/com/company/QuickSort.java
+++ src/com/company/QuickSort.java
@@ -2,29 +2,29 @@ package com.company;
 
 public class QuickSort {
 
-    public static void quickSort(int arr[], int begin, int end) {
-        if (begin<end) {
-            int partition = partition(arr, begin, end);
-            quickSort(arr, 0, partition-1);
-            quickSort(arr, partition+1, end);
-        }
+    public static void quickSort(int[] arr, int begin, int end) {
+    if (begin<end) {
+        int partition = partition(arr, begin, end);
+        quickSort(arr, 0,partition-1);
+        quickSort(arr, partition+1, end);
+    }
     }
 
     private static int partition(int[] arr, int begin, int end) {
         int pivot = arr[end];
-        int i = begin-1;
+        int i = begin - 1;
         for (int k=begin; k<end; k++) {
             if (arr[k]<=pivot) {
                 i++;
-                int swapTemp = arr[k];
-                arr[k]=arr[i];
-                arr[i]=swapTemp;
+                int tempSwap = arr[i];
+                arr[i]=arr[k];
+                arr[k]=tempSwap;
             }
         }
-        int swapTemp = arr[i+1];
-        arr[i+1]=arr[end];
-        arr[end]=swapTemp;
-        return i+1;
+        int tempSwap = arr[++i];
+        arr[i]=arr[end];
+        arr[end]=tempSwap;
+        return i;
     }
 
 }
